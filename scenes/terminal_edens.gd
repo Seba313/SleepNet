adf52ad5c7b4488e70dd5e6ff27693f2cf8b6929
Seba <sebastian.pasich@gmail.com>
2021-07-16 14:37:18 +0200

    terminal
    Login and password mechanic

--- scenes/terminal_edens.gd
+++ scenes/terminal_edens.gd
@@ -2,8 +2,7 @@ extends Control
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var active_user = ""
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,8 +12,38 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $CheckBox.pressed == true:
-		$CheckBox2.disabled = true
-	else:
-		$CheckBox2.disabled = false
-#	pass
+	if $Audio_WRONG.playing == true:
+		var pos = $Audio_WRONG.get_playback_position()
+		if pos > 0.3:
+			$Audio_WRONG.stop()
+	if $Audio_RIGHT.playing == true:
+		var pos = $Audio_RIGHT.get_playback_position()
+		if pos > 0.3:
+			$Audio_RIGHT.stop()
+
+
+
+
+func _on_Check_RYAN_pressed():
+	active_user = "RYAN"
+	if $Check_EDEN.pressed == true:
+		$Check_EDEN.pressed = false
+
+func _on_Check_EDEN_pressed():
+	active_user = "EDEN"
+	if $Check_RYAN.pressed == true:
+		$Check_RYAN.pressed = false
+
+
+
+func _on_Button_pressed():
+	if active_user == "RYAN" and $LineEdit.text != "1234":
+		$Audio_WRONG.play()
+	elif active_user == "RYAN" and $LineEdit.text == "1234":
+		$Audio_RIGHT.play()
+		
+	if active_user == "EDEN" and $LineEdit.text != "1234":
+		$Audio_WRONG.play()
+	elif active_user == "EDEN" and $LineEdit.text == "1234":
+		$Audio_RIGHT.play()
+
